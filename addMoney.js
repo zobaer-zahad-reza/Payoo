1a51a79bc00bbaba64dc8356209ed1a445838b03
Zobaer <zobaerzahad.git@gmail.com>
2025-02-26 01:29:08 +0600

    added add money section full

---
+++ addMoney.js
@@ -0,0 +1,20 @@
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const amount = parseFloat(document.getElementById("amount").value);
+    const pin = parseInt(document.getElementById("ac-pin").value);
+    const mainBalance = parseFloat(
+      document.getElementById("main-balance").innerText
+    );
+    if (amount && pin) {
+      if (pin === 1234) {
+        const sum = mainBalance + amount;
+        document.getElementById("main-balance").innerText = sum;
+      } else {
+        alert("Plz enter 1234 as a pin.");
+      }
+    } else {
+      alert("Enter amount");
+    }
+  });
