7ec72d09ab081b342cfebe0d45dc1807036837f1
Zobaer <zobaerzahad.git@gmail.com>
2025-02-25 14:41:47 +0600

    done login page

--- script/login.js
+++ script/login.js
@@ -1,3 +1,16 @@
-document.getElementById("login-btn").addEventListener("click", function () {
-  console.log("btn clicked");
-});
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const acNum = document.getElementById("ac-num").value;
+    const acPin = document.getElementById("ac-pin").value;
+    if (acNum.length === 11) {
+      if (parseInt(acPin) === 1234) {
+        window.location.href = "main.html";
+      } else {
+        alert("plz enter 1234 as a pin");
+      }
+    } else {
+      alert("invalid account number");
+    }
+  });
