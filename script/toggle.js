acdccdb827279d7bd0cf3671a07de812da110c24
Zobaer <zobaerzahad.git@gmail.com>
2025-02-26 14:29:54 +0600

    halka edit in toggle file

--- script/toggle.js
+++ script/toggle.js
@@ -3,24 +3,30 @@ document.getElementById("cashout-container").style.display = "none";
 document.getElementById("transfer-money-container").style.display = "none";
 document.getElementById("Bonus-container").style.display = "none";
 
+// for add money
 document.getElementById("add-money-box").addEventListener("click", function () {
   document.getElementById("addMoney-container").style.display = "block";
   document.getElementById("cashout-container").style.display = "none";
+  document.getElementById("transfer-money-container").style.display = "none";
+  document.getElementById("Bonus-container").style.display = "none";
 });
-
+// for cashout
 document.getElementById("cashout-box").addEventListener("click", function () {
   document.getElementById("cashout-container").style.display = "block";
   document.getElementById("addMoney-container").style.display = "none";
+  document.getElementById("transfer-money-container").style.display = "none";
+  document.getElementById("Bonus-container").style.display = "none";
 });
-
+// for transfer money
 document
   .getElementById("transfer-money-box")
   .addEventListener("click", function () {
+    document.getElementById("transfer-money-container").style.display = "block";
     document.getElementById("cashout-container").style.display = "none";
     document.getElementById("addMoney-container").style.display = "none";
-    document.getElementById("transfer-money-container").style.display = "block";
+    document.getElementById("Bonus-container").style.display = "none";
   });
-
+// for bonus
 document.getElementById("bonus-box").addEventListener("click", function () {
   document.getElementById("cashout-container").style.display = "none";
   document.getElementById("addMoney-container").style.display = "none";
