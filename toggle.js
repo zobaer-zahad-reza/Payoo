1b914cf7394f7eb9b9e5685057aad6366eaa63da
Zobaer <zobaerzahad.git@gmail.com>
2025-02-26 12:16:07 +0600

    added cashout & transfer ui

---
+++ toggle.js
@@ -0,0 +1,21 @@
+document.getElementById("addMoney-container").style.display = "none";
+document.getElementById("cashout-container").style.display = "none";
+document.getElementById("transfer-money-container").style.display = "none";
+
+document.getElementById("add-money-box").addEventListener("click", function () {
+  document.getElementById("addMoney-container").style.display = "block";
+  document.getElementById("cashout-container").style.display = "none";
+});
+
+document.getElementById("cashout-box").addEventListener("click", function () {
+  document.getElementById("cashout-container").style.display = "block";
+  document.getElementById("addMoney-container").style.display = "none";
+});
+
+document
+  .getElementById("transfer-money-box")
+  .addEventListener("click", function () {
+    document.getElementById("cashout-container").style.display = "none";
+    document.getElementById("addMoney-container").style.display = "none";
+    document.getElementById("transfer-money-container").style.display = "block";
+  });
