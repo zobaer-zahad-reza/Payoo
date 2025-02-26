d6a28388f04890809269bcace803a25ae805c412
Zobaer <zobaerzahad.git@gmail.com>
2025-02-26 18:22:04 +0600

    again edit

--- script/toggle.js
+++ script/toggle.js
@@ -1,35 +1,60 @@
-document.getElementById("addMoney-container").style.display = "none";
-document.getElementById("cashout-container").style.display = "none";
-document.getElementById("transfer-money-container").style.display = "none";
-document.getElementById("Bonus-container").style.display = "none";
+displayNoneById("addMoney-container");
+displayNoneById("cashout-container");
+displayNoneById("transfer-money-container");
+displayNoneById("Bonus-container");
 
 // for add money
 document.getElementById("add-money-box").addEventListener("click", function () {
   document.getElementById("addMoney-container").style.display = "block";
-  document.getElementById("cashout-container").style.display = "none";
-  document.getElementById("transfer-money-container").style.display = "none";
-  document.getElementById("Bonus-container").style.display = "none";
+  displayNoneById("cashout-container");
+  displayNoneById("transfer-money-container");
+  displayNoneById("Bonus-container");
 });
 // for cashout
 document.getElementById("cashout-box").addEventListener("click", function () {
   document.getElementById("cashout-container").style.display = "block";
-  document.getElementById("addMoney-container").style.display = "none";
-  document.getElementById("transfer-money-container").style.display = "none";
-  document.getElementById("Bonus-container").style.display = "none";
+  displayNoneById("addMoney-container");
+  displayNoneById("transfer-money-container");
+  displayNoneById("Bonus-container");
 });
 // for transfer money
 document
   .getElementById("transfer-money-box")
   .addEventListener("click", function () {
     document.getElementById("transfer-money-container").style.display = "block";
-    document.getElementById("cashout-container").style.display = "none";
-    document.getElementById("addMoney-container").style.display = "none";
-    document.getElementById("Bonus-container").style.display = "none";
+    displayNoneById("cashout-container");
+    displayNoneById("addMoney-container");
+    displayNoneById("Bonus-container");
   });
 // for bonus
 document.getElementById("bonus-box").addEventListener("click", function () {
-  document.getElementById("cashout-container").style.display = "none";
-  document.getElementById("addMoney-container").style.display = "none";
-  document.getElementById("transfer-money-container").style.display = "none";
   document.getElementById("Bonus-container").style.display = "block";
+  displayNoneById("cashout-container");
+  displayNoneById("addMoney-container");
+  displayNoneById("transfer-money-container");
 });
+
+// for pay bill
+document.getElementById("pay-bill-box").addEventListener("click", function () {
+  displayNoneById("addMoney-container");
+  displayNoneById("cashout-container");
+  displayNoneById("transfer-money-container");
+  displayNoneById("Bonus-container");
+  alert("This option is under construction...");
+});
+
+// for transactions
+document
+  .getElementById("transactions-box")
+  .addEventListener("click", function () {
+    displayNoneById("addMoney-container");
+    displayNoneById("cashout-container");
+    displayNoneById("transfer-money-container");
+    displayNoneById("Bonus-container");
+    alert("This option is under construction...");
+  });
+
+// make extra function for display none
+function displayNoneById(id) {
+  document.getElementById(id).style.display = "none";
+}
